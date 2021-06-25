6303cf5d6e6127e0464c03c601b3ce39f5f38bab
mohamed-linagora <mohamed.benali91@yahoo.fr>
2021-06-25 15:19:40 +0100

    editing

--- utf8_to_buckwalter.py
+++ utf8_to_buckwalter.py
@@ -11,7 +11,7 @@ with open(sys.argv[1], 'r') as f:       # input your text file
     contents = f.readlines()
     for content in contents:
         buck_format = buckwalter.transliterate(content)    # transliterate ligne by ligne
-        output_res = open("result.txt", "a")       # output result file
+        output_res = open(sys.argv[2], "a")       # output result file
         output_res.writelines(buck_format)
     output_res.close()
 
